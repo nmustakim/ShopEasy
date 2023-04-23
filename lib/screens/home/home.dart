f4606f55910a983c75f8c0ea97640015c2c5cac1
Nayeem Mustakim <nmustakim98@gmail.com>
2023-04-23 16:25:24 +0600

    drawer,cart

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:shopeasy/constants.dart';
 import 'package:shopeasy/repositories/product_repository.dart';
+import 'package:shopeasy/screens/cart/cart.dart';
 import 'package:shopeasy/screens/dairy/dairy.dart';
 import 'package:shopeasy/screens/fruits/fruits.dart';
 import 'package:shopeasy/screens/meat/meat.dart';
@@ -21,6 +22,7 @@ class _HomeState extends State<Home> {
   ProductRepository? _productRepository;
   List<Product>? productsList;
   List<Product>? productsListContainer;
+  final scaffoldKey = GlobalKey<ScaffoldState>();
 
   FirebaseFirestore firestore = FirebaseFirestore.instance;
   @override
@@ -52,107 +54,153 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        automaticallyImplyLeading: false,
-        title: Text(
-          "Products",
-          style: titleTextStyle1,
-        ),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-        Stack(
-        children: [
-        Container(
-        height: 120,
-        decoration: BoxDecoration(
-            gradient: const LinearGradient(
-                colors: [Color(0XFFE2E9FF), Color(0XFFFFEAEA)]),
-            borderRadius: BorderRadius.circular(16)),
-      ),
-          Padding(
-            padding: const EdgeInsets.all(24),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Free shipping',
-                    style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.w700,
-                        fontFamily: GoogleFonts.montserrat().fontFamily,
-                        color: Colors.red)),
-                const SizedBox(
-                  height: 4,
-                ),
-                Row(
-                  children: [
-                    Text('When ordering',
-                        style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w700,
-                            fontFamily:
-                            GoogleFonts.montserrat().fontFamily,
-                            color: const Color(0XFF000000))),
-                    const Expanded(
-                      child: SizedBox(
-
-                      ),
-                    ),
-                    Container(
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(4),
-                          color: const Color(0XFFFFBB56)),
-                      child: Padding(
-                        padding: const EdgeInsets.fromLTRB(8, 3, 8, 3),
-                        child: Text('From 50\$',
-                            style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.w500,
-                                fontFamily:
-                                GoogleFonts.montserrat().fontFamily,
-                                color: const Color(0XFF000000))),
-                      ),
-                    ),
-                  ],
-                ),
+            key: scaffoldKey,
+            backgroundColor: Colors.white,
+            appBar: AppBar(
+              leading: IconButton(
+                color: Colors.red,
+                icon: const Icon(Icons.menu),
+                onPressed: () {
+                  if (scaffoldKey.currentState!.isDrawerOpen) {
+                    scaffoldKey.currentState!.closeDrawer();
+                    //close drawer, if drawer is open
+                  } else {
+                    scaffoldKey.currentState!.openDrawer();
+                    //open drawer, if drawer is closed
+                  }
+                },
+              ),
+              actions: [
+                IconButton(
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Cart()));
+                    },
+                    icon: const Icon(
+                      Icons.shopping_cart_outlined,
+                      color: Colors.redAccent,
+                    ))
               ],
+              backgroundColor: Colors.white,
+              elevation: 0,
+              centerTitle: true,
+              title: Text(
+                "Products",
+                style: titleTextStyle1,
+              ),
             ),
-          ),
-          ]
-        ),
-            const SizedBox(height: 25,),
-            Expanded(
-              child: GridView.builder(
-                  itemCount: productsList!.length,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2),
-                  itemBuilder: (context, index) {
-                    Product data = productsList![index];
-                    return InkWell(
-                      onTap: () {
-                        const List<Widget> screens = [Fruits(),Vegetables(),Dairies(),Meats()];
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => screens[index]));
-                      },
-                      child: GridTile(
-                          child: CategoryItemCard(
-                              image: data.image, name: data.name)),
-                    );
-                  }),
+            drawer: Drawer(
+              child: ListView(
+                children: const [
+                  ListTile(
+                    tileColor: Colors.white60,
+                  ),
+                  ListTile(
+                    tileColor: Colors.white60,
+                  ),
+                  ListTile(
+                    tileColor: Colors.white60,
+                  ),
+                  ListTile(
+                    tileColor: Colors.white60,
+                  ),
+                ],
+              ),
             ),
-          ],
-
-      ),
-    )
-        )
-    );
+            body: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Stack(children: [
+                    Container(
+                      height: 120,
+                      decoration: BoxDecoration(
+                          gradient: const LinearGradient(
+                              colors: [Color(0XFFE2E9FF), Color(0XFFFFEAEA)]),
+                          borderRadius: BorderRadius.circular(16)),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(24),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Free shipping',
+                              style: TextStyle(
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.w700,
+                                  fontFamily:
+                                      GoogleFonts.montserrat().fontFamily,
+                                  color: Colors.red)),
+                          const SizedBox(
+                            height: 4,
+                          ),
+                          Row(
+                            children: [
+                              Text('When ordering',
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w700,
+                                      fontFamily:
+                                          GoogleFonts.montserrat().fontFamily,
+                                      color: const Color(0XFF000000))),
+                              const Expanded(
+                                child: SizedBox(),
+                              ),
+                              Container(
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(4),
+                                    color: const Color(0XFFFFBB56)),
+                                child: Padding(
+                                  padding:
+                                      const EdgeInsets.fromLTRB(8, 3, 8, 3),
+                                  child: Text('From 50\$',
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.w500,
+                                          fontFamily: GoogleFonts.montserrat()
+                                              .fontFamily,
+                                          color: const Color(0XFF000000))),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ]),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  Expanded(
+                    child: GridView.builder(
+                        itemCount: productsList!.length,
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 2),
+                        itemBuilder: (context, index) {
+                          Product data = productsList![index];
+                          return InkWell(
+                            onTap: () {
+                              const List<Widget> screens = [
+                                Fruits(),
+                                Vegetables(),
+                                Dairies(),
+                                Meats()
+                              ];
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => screens[index]));
+                            },
+                            child: GridTile(
+                                child: CategoryItemCard(
+                                    image: data.image, name: data.name)),
+                          );
+                        }),
+                  ),
+                ],
+              ),
+            )));
   }
 }
