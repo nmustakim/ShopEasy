52c12df92667a6db25196a6ad2e8bab8282255e6
Nayeem Mustakim <nmustakim98@gmail.com>
2023-09-13 17:43:35 +0600

    login validation updated

--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -1,5 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:shopeasy/constants.dart';
+import 'package:shopeasy/firebase_helpers/firebaseAuth_helper.dart';
+import 'package:shopeasy/global_widgets/bottom_appbar.dart';
 import 'package:shopeasy/screens/forgot_password/forgot_password.dart';
 import '../../constants.dart';
 import '../../firebase_helpers/firebaseAuth_helper.dart';
@@ -19,150 +22,163 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   bool isObscure = true;
 
-  TextEditingController? emailController;
-  TextEditingController? passwordController;
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
   @override
-  void initState() {
-    emailController = TextEditingController();
-    passwordController = TextEditingController();
-    super.initState();
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
   }
 
-  signIn() async {
-    try {
-      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
-          email: emailController!.text, password: passwordController!.text);
-      var authCredential = credential.user;
-      if (authCredential!.uid.isNotEmpty) {
-        Navigator.pushReplacement(context,
-            MaterialPageRoute(builder: (context) => const BottomBar()));
-      }
-    } on FirebaseAuthException catch (e) {
-      if (e.code == 'user-not-found') {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: const Text("User not found"),
-            action: SnackBarAction(
+  Widget buildPasswordField() {
+    return SizedBox(
+      height: 50,
+      child: TextField(
+        obscureText: isObscure,
+        controller: passwordController,
+        decoration: InputDecoration(
+          hintText: "Password",
+          suffixIcon: InkWell(
+            onTap: () {
+              setState(() {
+                isObscure = !isObscure;
+              });
+            },
+            child: const Icon(Icons.remove_red_eye_sharp),
+          ),
+          filled: true,
+          fillColor: Colors.white,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> signIn() async {
+    final String email = emailController.text;
+    final String password = passwordController.text;
+
+    if (_validateInput(email, password)) {
+      try {
+        showLoaderDialog(context);
+        final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+        var authCredential = credential.user;
+        if (authCredential!.uid.isNotEmpty) {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const BottomBar(),
+            ),
+          );
+        }
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'user-not-found') {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: const Text("User not found"),
+              action: SnackBarAction(
                 label: 'Cancel',
                 onPressed: () {
                   Navigator.pop(context);
-                }),
-          ),
-        );
-      } else if (e.code == 'wrong-password') {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: const Text("Wrong password!"),
-            action: SnackBarAction(
+                },
+              ),
+            ),
+          );
+        } else if (e.code == 'wrong-password') {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: const Text("Wrong password!"),
+              action: SnackBarAction(
                 label: 'Cancel',
                 onPressed: () {
                   Navigator.pop(context);
-                }),
-          ),
-        );
+                },
+              ),
+            ),
+          );
+        }
       }
     }
   }
 
+
+
   @override
   Widget build(BuildContext context) {
     return ReusableBodyPart(
-        topMargin: 120,
-        childWidget: Padding(
-          padding: const EdgeInsets.all(8),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text("Sign In", style: titleTextStyle1),
-              const SizedBox(
-                height: 8,
-              ),
-              Text(
-                  "Welcome back to explore the pure & fresh groceries of ShopEasy",
-                  style: bodyTextStyle2),
-              const SizedBox(
-                height: 12,
-              ),
-              SizedBox(
-                  height: 50,
-                  child: TextField(
-                      controller: emailController,
-                      decoration: InputDecoration(
-                          filled: true,
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(12))))),
-              const SizedBox(
-                height: 8,
-              ),
-              SizedBox(
-                  height: 50,
-                  child: TextField(
-                      obscureText: isObscure,
-                      controller: passwordController,
-                      decoration: InputDecoration(
-                          suffixIcon: InkWell(
-                              onTap: () {
-                                setState(() {
-                                  isObscure = !isObscure;
-                                });
-                              },
-                              child: const Icon(Icons.remove_red_eye_sharp)),
-                          filled: true,
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(12))))),
-              const SizedBox(
-                height: 20,
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: BottomButton(
-                        buttonName: 'Log in',
-                        onPressed: () async {
-                          bool isValidated = loginVaildation(
-                              emailController!.text, passwordController!.text);
-                          if (isValidated == true) {
-                            bool isLoggedIn = await FirebaseAuthHelper
-                                .firebaseAuthHelper
-                                .login(emailController!.text,
-                                    passwordController!.text, context);
-                            if (isLoggedIn == true) {
-                              Future.delayed(const Duration(seconds: 1), () {
-                                Navigator.pushAndRemoveUntil(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) =>
-                                            const BottomBar()),
-                                    (route) => false);
-                              });
-                            }
-                          }
-                        }),
+      topMargin: 120,
+      childWidget: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Sign In", style: titleTextStyle1),
+            const SizedBox(height: 8),
+            Text(
+              "Welcome back to explore the pure & fresh groceries of ShopEasy",
+              style: bodyTextStyle2,
+            ),
+            const SizedBox(height: 12),
+            SizedBox(
+              height: 50,
+              child: TextField(
+                controller: emailController,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
                   ),
-                ],
+                ),
               ),
-              TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const ForgotPassword()));
-                  },
-                  child: Text(
-                    "Forgot password?",
-                    style: bodyTextStyle1.copyWith(color: Colors.red),
-                  )),
-              Padding(
-                padding: const EdgeInsets.only(top: 25),
-                child: BottomRow(
-                    navigatingScreen: const Registration(),
-                    bottomText: "Don't have account?",
-                    buttonName: "Sign Up"),
+            ),
+            const SizedBox(height: 8),
+            buildPasswordField(),
+            const SizedBox(height: 20),
+            Row(
+              children: [
+                Expanded(
+                  child: BottomButton(
+                    buttonName: 'Log in',
+                    onPressed: () async {
+                      signIn();
+                    },
+                  ),
+                ),
+              ],
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const ForgotPassword(),
+                  ),
+                );
+              },
+              child: Text(
+                "Forgot password?",
+                style: bodyTextStyle1.copyWith(color: Colors.red),
               ),
-            ],
-          ),
-        ));
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 25),
+              child: BottomRow(
+                navigatingScreen: const Registration(),
+                bottomText: "Don't have an account?",
+                buttonName: "Sign Up",
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/utils/login_utils.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+bool _validateInput(String email, String password,context) {
+
+  if (email.isEmpty || password.isEmpty) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: const Text("Please fill in all fields."),
+        action: SnackBarAction(
+          label: 'Cancel',
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
+    return false;
+  }
+  return true;
+}
